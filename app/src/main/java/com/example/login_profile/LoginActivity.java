e3a930eb295f8e396267d9dde5b116fba18c8531
TranXuanTruong205 <truong2005tran@gmail.com>
2025-10-05 21:57:52 +0700

    Create Login Activity

---
+++ app/src/main/java/com/example/login_profile/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.login_profile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn_2339 = findViewById(R.id.txtSignUp);
+
+    }
+}
